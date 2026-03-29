17a8a62d8128b045ae3b3a90d879dc74ddd54cbc
MikeyNomad <datnopegay@gmail.com>
2026-03-29 20:41:28 +0700

    BT3

---
+++ BT3/BT3/Views/UC_GIao_Dien.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BT3.Views
+{
+    /// <summary>
+    /// Interaction logic for UC_GIao_Dien.xaml
+    /// </summary>
+    public partial class UC_GIao_Dien : Window
+    {
+        public  partial class UC_Giao_Dien
+        {
+            
+        }
+    }
+}
